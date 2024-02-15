026a0e0be54b2782846cce8e8ea8d5c38efb5d21
Ali Husnain <139127727+alihusnain010203@users.noreply.github.com>
2024-02-15 14:21:40 +0500

    show data in table and also add delete the post API

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import {verifyUser} from '../utils/verifyUser.js'
-import { createPost,getAllPostofAdmin } from '../controller/post.controller.js';
+import { createPost,getAllPostofAdmin,deletePost  } from '../controller/post.controller.js';
 const router = express.Router();
 
 router.post("/createpost",verifyUser,createPost);
 
 // get all posts
 
-router.get("/getallpost",verifyUser,getAllPostofAdmin);
+router.get("/getallpost",getAllPostofAdmin);
+
+router.delete("/deletepost/:postId/:userId",verifyUser,deletePost);
+
+// router.put("/editpost",editPost);
 
 
 export default router;
